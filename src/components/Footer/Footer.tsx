3ab4934f031ffc47fc3fab8222ac1b7d18745068
bocal <bocal@bocal-xitope.42.fr>
2018-12-15 04:09:47 +0200

    push

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,27 +1,21 @@
-import { withRouter } from 'next/router';
-
 import * as React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
 import * as css from './Footer.scss';
 
-
 class Footer extends React.Component {
   render() {
 
     return (
-      <Container style={{ marginTop: '100px', height: '200px' }} className='d-flex align-items-center '>
+      <Container fluid style={{ marginTop: '100px', height: '200px' }} className='d-flex align-items-center justify-content-center'>
         <footer className={css.footer}>
-          <Row className="justify-content-between">
-            <Col sm={4}>
+          <Row className="justify-content-between pb-4">
+            <Col sm={4} style={{height: '120px'}}>
               <h1 className={css.logo}>Pencil.</h1>
-              <span>
-                The process of launching your own desing studio or web agency is very time consuming.
-              </span>
             </Col>
 
             <Col sm={7} className="d-flex align-items-center ml-2">
-              <Container style={{height: "100%", display: 'flex', flexDirection: 'column' ,justifyContent: 'space-around'}}>
+              <Container style={{height: "80%", display: 'flex', flexDirection: 'column' ,justifyContent: 'space-around'}}>
                 <Row className="d-flex justify-content-around">
                   <a style={{ width: "25%" }}>Facultati</a>
                   <a style={{ width: "25%" }}>Cumpara un curs</a>
@@ -40,14 +34,12 @@ class Footer extends React.Component {
             </Col>
           </Row>
 
-          <hr />
+          <hr style={{borderWidth: '1px solid black', width: '100%'}} />
 
-          <Row>
+          <Row className="pb-4 "> 
             <Col sm={9}>
               <a style={{ fontSize: '15px', paddingRight: '25px' }}>Terms of service</a>
               <a style={{ fontSize: '15px', paddingRight: '25px' }}>Privacy policy</a>
-              <a style={{ fontSize: '15px', paddingRight: '25px' }}>Embed slack</a>
-              <a style={{ fontSize: '15px', paddingRight: '25px' }}>Slack chat widget</a>
             </Col>
             <Col sm={3}>
               <span style={{ fontSize: '15px' }}>
