ba2a6ef0abc851ef087e881e7909408f626b6d94
alex <new.nigol.42@gmail.com>
2018-12-15 05:01:59 +0200

    export!

--- src/services/utils/fileToBase64.ts
+++ src/services/utils/fileToBase64.ts
@@ -1,4 +1,4 @@
-function getBase64(file: File): Promise<string> {
+export function getBase64(file: File): Promise<string> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
